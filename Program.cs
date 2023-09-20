02410bd2283ac530d7c85c1c5778279b644c89c1
zabyl <zabylshag@gmail.com>
2023-09-20 17:01:12 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Lütfen dairenin çapını giriniz: ");
+        double radius = Convert.ToDouble(Console.ReadLine());
+        double thickness = 0.4;
+        ConsoleColor BorderColor = ConsoleColor.Yellow;
+        Console.ForegroundColor = BorderColor;
+        char symbol = '*';
+ 
+        double rIn = radius - thickness, rOut = radius + thickness;
+      
+        for (double y = radius; y >= -radius; --y)
+            {
+                for (double x = -radius; x < rOut; x += 0.5)
+                {
+                    double value = x * x + y * y;
+                    if (value >= rIn * rIn && value <= rOut * rOut)
+                    {
+                        Console.Write(symbol);
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+    }
+}
